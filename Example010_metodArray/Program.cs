52ff96391d388515e310ca93174c8d32e9c239ca
pasha <Paulcivicpeppi@mail.ru>
2022-04-12 23:06:46 +0300

    Index array

---
+++ Example010_metodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 23, 54, 65, 45, 545, 546, 36 };
+
+int n = array.Length;
+int find = 545;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+
+    // index = index +1;
+    index++;
+}
